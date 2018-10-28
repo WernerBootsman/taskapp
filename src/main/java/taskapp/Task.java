f0163c79fd45ea26259204d0ae737c6a5452793f
Werner Bootsman <werner.bootsman@quicknet.nl>
2018-10-28 12:37:24 +0100

    - Refactored from Array to ArrayList - Added unique ID for a Task - Override for toString to return task ID and name
    To do:
    - Create endpoint to GET Task(s)
    - Create PUT, DELETE endpoints

--- src/main/java/taskapp/Task.java
+++ src/main/java/taskapp/Task.java
@@ -1,18 +1,32 @@
 package taskapp;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Task {
 
     //The Task Class has these fields
-    public String taskName;
+    String taskName;
+    private static AtomicInteger uniqueId=new AtomicInteger();
+    public int id;
+
 
     //This is the constructor of the Task Class
     public Task(String name) {
+        id=uniqueId.getAndIncrement();
         taskName = name;
     }
 
-    public String toString(){
-        return "Task name:" + taskName;
+    //Method that returns the name of a Task
+    public String getTaskname(){
+        return taskName;
     }
+
+    //This overrides the default toString method, to return the ID and name of a task, instead of the default Object and hashcode
+    @Override
+    public String toString() {
+        return "Task ID: " + this.id + " Task name:" + this.getTaskname();
+    }
+
 }
 
 
--- src/main/java/taskapp/main.java
+++ src/main/java/taskapp/main.java
@@ -3,7 +3,7 @@ import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 @RestController
@@ -19,21 +19,26 @@ public class main {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(main.class, args);
 
-        Task[] taskArray;  // declares a variable 'taskArray' of type Task array.
-        taskArray = new Task[5];  // creates an array to hold 5 Tasks.
+        //create an array list.
+        ArrayList<Task> TaskList = new ArrayList<>();
+        System.out.println("Initial size of TaskList:" + TaskList.size());
 
-        int count = 0;
 
         do {
             Scanner input = new Scanner(System.in);
-            System.out.println("Enter a task");
+            System.out.println("Enter a task: ");
             String myString = input.nextLine();
-            System.out.println("You entered:" + myString);
-            taskArray[count++] = new Task(myString);
-        } while (count <5);
+            System.out.println("You entered: " + myString);
+            TaskList.add(new Task (myString));
+            System.out.println("Current size of ArrayList: " + TaskList.size());
+            System.out.println("The current list of tasks: " + TaskList.toString());
+        } while (true);
 
-        System.out.println(Arrays.toString(taskArray));
-
+        //TODO
+        //- Create GET endpoint to retrieve Task(s)
+        //- Create endpoint to create a Task
+        //- Create endpoint to list all Tasks
+        //- Create andpoint to delete a specific Task
     }
 
 }
